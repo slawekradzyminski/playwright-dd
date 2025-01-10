61fda5d2957afb380ab3908fc6b74af25f115c85
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-01-10 14:29:45 +0100

    Enable automatic screenshots on test failures

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
+    /* Take screenshot on failure */
+    screenshot: 'only-on-failure',
+
     extraHTTPHeaders: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
